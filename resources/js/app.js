c7c93e6a47377de669190e6f034943d0d0984f0a
Rizki Darmawan <80609220+rizkidarmawan21@users.noreply.github.com>
2024-12-22 09:57:52 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,22 +9,28 @@ Alpine.plugin(focus)
 
 Alpine.start();
 
-// DARK MODE TOGGLE BUTTON
+// DARK MODE TOGGLE - Set default theme first
+localStorage.setItem("color-theme", "dark");
+
 var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
+// Check and apply the correct theme based on localStorage or user preferences
 if (
     localStorage.getItem("color-theme") === "dark" ||
     (!("color-theme" in localStorage) &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
+    document.documentElement.classList.add("dark");
     themeToggleLightIcon.classList.remove("hidden");
 } else {
+    document.documentElement.classList.remove("dark");
     themeToggleDarkIcon.classList.remove("hidden");
 }
 
 var themeToggleBtn = document.getElementById("theme-toggle");
 
+// Toggle theme on button click
 themeToggleBtn.addEventListener("click", function () {
     themeToggleDarkIcon.classList.toggle("hidden");
     themeToggleLightIcon.classList.toggle("hidden");
